776eae6d517b2e8627511679590d70072ecce32d
Iker Ibarguren Berasaluze <ikerib@gmail.com>
2019-09-28 00:29:03 +0200

    CRUD frm called form submit button

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,3 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
-
-
-
 
 
 const $ = require('jquery');
@@ -23,4 +11,7 @@ require('bootstrap');
 
 $(document).ready(function() {
     $('[data-toggle="popover"]').popover();
+    $('#btnSave').on('click', function (  ) {
+        $('#frm').submit();
+    })
 });
