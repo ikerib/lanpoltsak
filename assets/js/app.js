cf3075a4183c5e3131e191d581f3ec2f67ae7e37
Iker Ibarguren Berasaluze <ikerib@gmail.com>
2019-09-28 13:07:38 +0200

    Fontawesome

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,9 +9,8 @@ const $ = require('jquery');
 // the bootstrap module doesn't export/return anything
 require('bootstrap');
 
-// or you can include specific pieces
-// require('bootstrap/js/dist/tooltip');
-// require('bootstrap/js/dist/popover');
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
 
 
 $(document).ready(function() {
